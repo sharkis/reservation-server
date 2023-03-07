e94aeed439ab2a57126320079e94dfe212a067e9
Albert Sharkis <sharkis@sharkis.org>
2023-03-07 08:30:45 -0700

    adding reservations

---
+++ TODO.md
@@ -0,0 +1,26 @@
+# reservation length
+party size|duration (minutes)
+---|---
+2|90
+4|120
+5+|150
+
+# customer info
+* name
+* phone
+* email
+
+# reservation details
+notes
+
+area
+- bar
+- patio
+- dining floor
+
+occasion
+- birthday
+- anniversary
+- date
+- business
+- celebration
---
+++ reservations.js
@@ -0,0 +1,68 @@
+"use strict";
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
+
+const createReservation = async (event) => {
+  const body = JSON.parse(Buffer.from(event.body, "base64").toString());
+  const date = +(body.timestamp / 86400).toFixed(0);
+  const putParams = {
+    TableName: process.env.DYNAMODB_RESERVATION_TABLE,
+    Item: {
+      day: date,
+      uuid: uuidv4(),
+      area: body.area,
+      customer: body.customer,
+      notes: body.notes,
+      occasion: body.occasion,
+      size: body.size,
+      timestamp: body.timestamp,
+    },
+  };
+  await dynamoDb.put(putParams).promise();
+  return {
+    statusCode: 200,
+  };
+};
+
+const getReservations = async (event) => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+  const scanParams = {
+    TableName: process.env.DYNAMODB_RESERVATION_TABLE,
+  };
+  const result = await dynamoDb.scan(scanParams).promise();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      total: result.Count,
+      items: await result.Items.map((reservation) => {
+        return {
+          area: body.area,
+          customer: body.customer,
+          notes: body.notes,
+          occasion: body.occasion,
+          size: body.size,
+          timestamp: body.timestamp,
+        };
+      }),
+    }),
+  };
+};
+
+const checkReservation = async (event) => {
+  const body = JSON.parse(Buffer.from(event.body, "base64").toString());
+  const scanParams = {
+    TableName: process.env.DYNAMODB_RESERVATION_TABLE,
+    Select: COUNT,
+  };
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+  const result = await dynamoDb.scan(scanParams).promise();
+  return {
+    statusCode: 200,
+  };
+};
+
+module.exports = {
+  checkReservation,
+  createReservation,
+  getReservations,
+};
