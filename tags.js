d2062cc64a699857912fb82218fa17c9d6897f7a
Albert Sharkis <sharkis@sharkis.org>
2023-07-11 15:50:33 -0600

    added tags create/delete

--- tags.js
+++ tags.js
@@ -3,22 +3,56 @@
 const AWS = require("aws-sdk");
 const { v4: uuidv4 } = require("uuid");
 
-const getTags  = async (event) =>{
+const getTags = async (event) => {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
   const scanParams = {
     TableName: process.env.DYNAMODB_TAGS_TABLE,
   };
+  const result = await dynamoDb.scan(scanParams).promise();
   return {
-    statusCode:200,
-    body:JSON.stringify({}),
+    statusCode: 200,
+    body: JSON.stringify({
+      total: result.Count,
+      items: result.Items.map((tag) => {
+        return {
+          uuid: tag.uuid,
+          name: tag.name,
+          color: tag.color,
+        };
+      }),
+    }),
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true,
     },
-  }
+  };
+};
 
-}
+const createTag = async (event) => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+  const body = JSON.parse(event.body);
+  const putParams = {
+    TableName: process.env.DYNAMODB_TAGS_TABLE,
+    Item: {
+      uuid: uuidv4(),
+      name: body.name,
+      color: body.color,
+    },
+  };
+  await dynamoDb.put(putParams).promise();
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      status: "OK",
+    }),
+  };
+};
 
 module.exports = {
-    getTags
-}
+  getTags,
+  createTag,
+};
